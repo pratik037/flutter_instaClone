cba83947035fc7ff92476a3ac1d3343ea4dde5ba
pratik037 <singhalpratik037@gmail.com>
2019-04-09 17:48:22 +0530

    Added Stories and feed

--- lib/insta_list.dart
+++ lib/insta_list.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/insta_story.dart';
 
 class InstaList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) => index == 0? SizedBox(
+        child: InstaStory(),
+      )
+       : Column(),
     );
   }
 }
